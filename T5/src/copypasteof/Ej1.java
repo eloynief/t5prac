6641538da0c7c7d51557504422016467990f5f14
enieves <enieves@106-29.info2>
2023-01-10 12:52:41 +0100

    terminar

---
+++ T5/src/copypasteof/Ej1.java
@@ -0,0 +1,21 @@
+package copypasteof;
+
+import java.util.Arrays;
+
+public class Ej1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int tabla[]= new int [15];
+		int a[],b[];
+		for (int i=0; i<tabla.length;i++) {
+			
+		}
+		a=Arrays.copyOfRange(tabla, 0, 15);
+		System.out.println(Arrays.toString(a));
+		if () {
+			
+		}
+	}
+
+}
