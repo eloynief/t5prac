f6cb757ffeadd284db49c4ed3caf660f8afed92c
enieves <enieves@106-29.info2>
2023-01-11 14:40:42 +0100

    ttt

--- T5/src/copypasteof/Ej1.java
+++ T5/src/copypasteof/Ej1.java
@@ -1,21 +1,33 @@
 package copypasteof;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Ej1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int tabla[]= new int [15];
-		int a[],b[];
+		//
+		int a[]=new int [15];
+		int b[];
+		int resta=1,resta2=1, guardar=0;
+		System.out.println("Introduzca 5 numeros por el teclado: ");
+		Scanner sc= new Scanner(System.in);
+		//crear los 15 valores que queremos poner
 		for (int i=0; i<tabla.length;i++) {
+			int num=sc.nextInt();
+			tabla[i]=num;
 			
+			if(i>4) {
+				guardar=tabla[tabla.length-resta];
+				a[resta2]=guardar;
+				resta++;
+				resta2++;
+			}
 		}
-		a=Arrays.copyOfRange(tabla, 0, 15);
+		
 		System.out.println(Arrays.toString(a));
-		if () {
-			
-		}
 	}
 
 }
