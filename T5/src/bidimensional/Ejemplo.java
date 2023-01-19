4af611ed33aba2323ce894ee9b1e0801b20e1794
enieves <enieves@106-02.info2>
2023-01-19 13:49:42 +0100

    Terminar yqa

---
+++ T5/src/bidimensional/Ejemplo.java
@@ -0,0 +1,16 @@
+package bidimensional;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] [] num= new int[20] [5];
+		for(int i=0; i<20;i++) {
+			for (int j=0; i<5;i++) {
+				System.out.println(num [i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
