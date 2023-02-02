962758e4e5ebad7c202e2d664b044225152cb8df
enieves <enieves@106-02.info2>
2023-02-02 13:29:08 +0100

    Ejercicio 6 Terminado

--- T5/src/bidimensional/Ej6.java
+++ T5/src/bidimensional/Ej6.java
@@ -10,27 +10,49 @@ Scanner sc= new Scanner(System.in);
 		
 		//
 		int[][] nums= new int[6][10];
-		int min=Integer.MAX_VALUE;
-		int max=0;
+		int minx=Integer.MAX_VALUE;
+		int maxx=0;
+		int miny=Integer.MAX_VALUE;
+		int maxy=0;
 		//crear variables
 		int i=0, j=0, sumafila=0;
+		int randomx=0;
+		int randomy=0;
 		
 		//para crear los valores en esto
 		for(i=0; i<6;i++) {
 			//
+			randomx=(int)(Math.random()*1000+1);
 			for (j=0; j<10;j++) {
-				
+				randomy=(int)(Math.random()*1000+1);
 				//el valor random
-				nums[i][j]=(int) (Math.random()*1000+1);
+				nums[i][j]=randomy;
 				//cada vez que el numero sea mas pequeño, la x y la y minimas
 				// cambian a la i y j respectivamente
+				if(randomy<miny) {
+					miny=randomy;
+				}
+				if(randomy>maxy) {
+					maxy=randomy;
+				}
 				
+			}
+			if(randomx<minx) {
+				minx=randomx;
+			}
+			if(randomx>maxx) {
+				maxx=randomx;
 			}
 			
 		}
 		//para esto se usa deep to string
 		System.out.println(Arrays.deepToString(nums));
-		
+		System.out.println("Los de y");
+		System.out.println(maxy);
+		System.out.println(miny);
+		System.out.println("Los de x");
+		System.out.println(maxx);
+		System.out.println(minx);
 		//crear el min
 		//crear el max
 		//crea variable para el minimo y el maximo
