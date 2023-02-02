613f085380cc175ddaf9442bd35b4209eee23046
enieves <enieves@106-02.info2>
2023-02-02 14:35:57 +0100

    terminar

---
+++ T5/src/funciones/Ej1.java
@@ -0,0 +1,19 @@
+package funciones;
+
+import java.util.Scanner;
+
+public class Ej1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		
+		//
+		int[][] nums= new int[6][5];
+		
+		//crear variables
+		int i=0, j=0, sumafila=0;
+		
+	}
+
+}
