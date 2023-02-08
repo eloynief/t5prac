cb1ae3a5d37b23a55cf408014eb25587b1000c81
enieves <enieves@106-02.info2>
2023-02-08 14:00:53 +0100

    terminar ya !!!

--- T5/src/funciones/Ej2.java
+++ T5/src/funciones/Ej2.java
@@ -5,23 +5,29 @@ import java.util.Scanner;
 
 public class Ej2 {
 
-	public int main(int t[]) {
+	public int man(int t[]) {
 		// TODO Auto-generated method stub
 		//while que el valor sea mayor que algo ese algo cambia de valor a ese numero
 		Scanner sc= new Scanner(System.in);
-		int[] tabla= new int [5];
 		int valor = 0;
 		int num=0;
-		for(int i=0;i<tabla.length;i++) {
+		for(int i=0;i<t.length;i++) {
 			//
-			tabla[num]=sc.nextInt();
+			t[num]=sc.nextInt();
 			//mientras que el valor puesto en la tabla es mayor que el valor, hace esto
-			if(tabla[num]>valor) {
-				valor=tabla[num];
+			if(t[num]>valor) {
+				valor=t[num];
 			}
 		System.out.println(valor);
 		}
 		return valor;
+	}
+	
+	public static void main(String[] args) {
+		int num;
+		
+		man(num);
+		
 	}
 	
 
